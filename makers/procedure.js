218fa83ff1b254ddea015e624f88ec74938855f6
Vinicius Mesquita <viniciusfmesquita@gmail.com>
2015-02-04 15:54:02 -0200

    New procedure template; New arguments for procedure creation

--- makers/procedure.js
+++ makers/procedure.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 ;(function(config) {
 
-  var argv = require('yargs').argv;  
+  var argv = require("yargs").argv;  
   var Handlebars = require('handlebars');
   var log = require("../utils/logger");
   var io = require("../utils/io");
   var path = require("path");
 
   if(Object.keys(argv).length < 4){
-    log.error('Check arguments passed (Usage: $ node index.js --t procedure --p NAME --e CUSTOMER [--d "DESCRIPTION"] [--o /path/to/output/])');
+    log.error('Check arguments passed (Usage: $ node index.js --t procedure --p NAME --k TEMPLATE_NAME [--e ENTITYNAME] [--d "DESCRIPTION"] [--o /path/to/output/])');
     return;
   }
   
-  var source = io.readFile(path.join(process.cwd(), 'templates/procedure.template'));
+  var kind = argv.k;
+  var source = io.readFile(path.join(process.cwd(), "templates", "procedure_" + kind + ".template"));
   var context = {
-      storedProcedure: argv.p
-    , entityId: (typeof argv.e !== 'undefined') ? argv.e.toLowerCase() : ''
-    , description: argv.d || 'Nao informado'
+      storedProcedure: proc(kind, argv.p)
+    , entityId: (typeof argv.e !== "undefined") ? argv.e.toLowerCase() : ""
+    , description: argv.d || "Nao informado"
     , now: new Date().toISOString()
   };
 
@@ -24,9 +25,13 @@
   var procedure = template(context);
   var path = fix(argv.o);
 
-  io.write(path + context.storedProcedure + '.sql', procedure);
+  io.write(path + context.storedProcedure + ".sql", procedure);
 
-  log.success('Script for procedure ' + context.storedProcedure + ' was created!');
+  log.success("Script for procedure " + context.storedProcedure + " was created!");
+
+  function proc(kind, name){
+    return kind.toUpperCase() + "_" + name;
+  }
 
   function fix(arg){
     return io.fixDirPath(arg, path.join(process.cwd(), "out/"));
