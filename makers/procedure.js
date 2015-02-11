30109bcc62a944dc3c41da7b822709ebac4c9d09
Vinicius Mesquita <viniciusfmesquita@gmail.com>
2015-02-11 12:28:06 -0200

    Improvements

--- makers/procedure.js
+++ makers/procedure.js
@@ -8,17 +8,25 @@
   var path = require("path");
 
   if(Object.keys(argv).length < 4){
-    log.error('Check arguments passed (Usage: $ node index.js --t procedure --p NAME --k TEMPLATE_NAME [--e ENTITYNAME] [--d "DESCRIPTION"] [--o /path/to/output/])');
+    log.error('Check arguments passed (Usage: $ node index.js --t procedure --p NAME --k TEMPLATE_NAME [--e ENTITYNAME] [--d "DESCRIPTION"] [--o /path/to/output/] [--p If present, will ignore the "k" param] [Ex.: --params "@Name nvarchar(200), @Date datetime"]])');
     return;
   }
   
+  var DEFAULT = "DEFAULT"
   var kind = argv.k;
+  var params = [];
+  if(argv.params){
+    params = argv.params.split(",");
+    kind = DEFAULT;
+  }
+
   var source = io.readFile(path.join(process.cwd(), "templates", "procedure_" + kind + ".template"));
   var context = {
       storedProcedure: proc(kind, argv.p)
     , entityId: (typeof argv.e !== "undefined") ? argv.e.toLowerCase() : ""
     , description: argv.d || "Nao informado"
     , now: new Date().toISOString()
+    , params: params
   };
 
   var template = Handlebars.compile(source);
@@ -30,6 +38,10 @@
   log.success("Script for procedure " + context.storedProcedure + " was created!");
 
   function proc(kind, name){
+    if(kind === DEFAULT){
+      return name;
+    }
+
     return kind.toUpperCase() + "_" + name;
   }
 
