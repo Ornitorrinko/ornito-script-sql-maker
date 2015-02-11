74b7cc4d7fa32337ad28bcffb59f5e8cd95bd96f
Vinicius Mesquita <viniciusfmesquita@gmail.com>
2015-02-11 11:40:48 -0200

    Bug fixing

--- makers/function.js
+++ makers/function.js
@@ -5,13 +5,14 @@
   var Handlebars = require('handlebars');
   var log = require("../utils/logger");
   var io = require("../utils/io");
+  var path = require("path");
 
   if(Object.keys(argv).length < 4){
     log.error('Check arguments passed (Usage: $ node index.js --t function --f NAME --r RETURN_TYPE(Varchar, int, bit etc...) [--d "DESCRIPTION"] [--o /path/to/output/] [--p [Ex.: --p "@Name nvarchar(200), @Date datetime"]])');
     return;
   }
   
-  var source = io.readFile(path.join(process.cwd(), 'templates/func.template'));
+  var source = io.readFile(path.join(process.cwd(), 'templates/function.template'));
   var params = [];
   if(argv.p){
     params = argv.p.split(",");
@@ -29,7 +30,7 @@
   var func = template(context);
   var path = fix(argv.o);
 
-  io.writeFile(path + context.func + '.sql', func);
+  io.write(path + context.func + '.sql', func);
 
   log.success('Script for function ' + context.func + ' was created!');
   
